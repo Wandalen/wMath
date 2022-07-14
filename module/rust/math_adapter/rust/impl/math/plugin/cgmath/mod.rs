fe00a00b512d625c0b77dd086d5c86505e7df345
dmvict <dm.vict.kr@gmail.com>
2022-07-14 09:56:58 +0300

    Update entry files of module `math_adapter`, use new version of macro `mod_interface`

--- module/rust/math_adapter/rust/impl/math/plugin/cgmath/mod.rs
+++ module/rust/math_adapter/rust/impl/math/plugin/cgmath/mod.rs
@@ -33,6 +33,12 @@ pub mod protected
 
 pub use protected::*;
 
+/// Orphan namespace of the module.
+pub mod orphan
+{
+  pub use super::exposed::*;
+}
+
 /// Exposed namespace of the module.
 pub mod exposed
 {
