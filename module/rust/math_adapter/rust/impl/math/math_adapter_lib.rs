40746f378326f9dbe4f0fbb2d4c7650b371edf47
dmvict <dm.vict.kr@gmail.com>
2022-07-18 10:49:57 +0300

    Update file `Readme.md`, add common badge, fix links

--- module/rust/math_adapter/rust/impl/math/math_adapter_lib.rs
+++ module/rust/math_adapter/rust/impl/math/math_adapter_lib.rs
@@ -1,7 +1,7 @@
 #![ cfg_attr( not( feature = "use_std" ), no_std ) ]
 #![ doc( html_logo_url = "https://raw.githubusercontent.com/Wandalen/wTools/master/asset/img/logo_v3_trans_square.png" ) ]
 #![ doc( html_favicon_url = "https://raw.githubusercontent.com/Wandalen/wTools/alpha/asset/img/logo_v3_trans_square_icon_small_v2.ico" ) ]
-#![ doc( html_root_url = "https://docs.rs/data_type/latest/math_adapter/" ) ]
+#![ doc( html_root_url = "https://docs.rs/math_adapter/latest/math_adapter/" ) ]
 #![ warn( rust_2018_idioms ) ]
 #![ warn( missing_debug_implementations ) ]
 #![ warn( missing_docs ) ]
