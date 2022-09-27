08a68b98ce0a06da61aea06f524c690d2dba285e
Serhii Chushyn <chushin.sergey@gmail.com>
2022-09-27 14:32:01 +0300

    Add 4 dimension tests for derive interfaces macro

--- rust/test/math/adapter/derive_vector_interfaces_test.rs
+++ rust/test/math/adapter/derive_vector_interfaces_test.rs
@@ -3,6 +3,7 @@ use math_adapter::
 {
   X2,
   X3,
+  X4,
 };
 use super::*;
 
@@ -31,13 +32,16 @@ struct TestX3Named{ x : u32, y : u32, z : u32 }
 struct TestX3NamedWithGenerics< S : ScalarInterface >{ x : S, y : S, z : S }
 
 #[ derive( VectorInterfaces ) ]
-struct TestTupleWithGenerics< S : ScalarInterface >( S, S );
+struct TestX4Tuple( u32, u32, u32, u32 );
 
 #[ derive( VectorInterfaces ) ]
-struct TestNamed{ x : u32, y : u32 }
+struct TestX4TupleWithGenerics< S : ScalarInterface >( S, S, S, S );
 
 #[ derive( VectorInterfaces ) ]
-struct TestNamedWithGenerics< S : ScalarInterface >{ x : S, y : S }
+struct TestX4Named{ x : u32, y : u32, z : u32, w : u32 }
+
+#[ derive( VectorInterfaces ) ]
+struct TestX4NamedWithGenerics< S : ScalarInterface >{ x : S, y : S, z : S, w : S }
 
 tests_impls!
 {
@@ -126,6 +130,50 @@ tests_impls!
     assert!( implements!( named => X3BasicInterface ) );
     assert!( implements!( named => X3CanonicalInterface ) );
   }
+
+  //
+
+  fn impls_x4_for_tuple()
+  {
+    let tuple = TestX4Tuple( 1, 2, 3, 4 );
+    assert!( implements!( tuple => X4NominalInterface ) );
+    assert!( implements!( tuple => X4Interface ) );
+    assert!( implements!( tuple => X4BasicInterface ) );
+    assert!( implements!( tuple => X4CanonicalInterface ) );
+  }
+
+  //
+
+  fn impls_x4_for_tuple_with_generics()
+  {
+    let tuple = TestX4TupleWithGenerics( 1, 2, 3, 4 );
+    assert!( implements!( tuple => X4NominalInterface ) );
+    assert!( implements!( tuple => X4Interface ) );
+    assert!( implements!( tuple => X4BasicInterface ) );
+    assert!( implements!( tuple => X4CanonicalInterface ) );
+  }
+
+  //
+
+  fn impls_x4_for_named()
+  {
+    let named = TestX4Named{ x : 1, y : 2, z : 3, w : 4 };
+    assert!( implements!( named => X4NominalInterface ) );
+    assert!( implements!( named => X4Interface ) );
+    assert!( implements!( named => X4BasicInterface ) );
+    assert!( implements!( named => X4CanonicalInterface ) );
+  }
+
+  //
+
+  fn impls_x4_for_named_with_generics()
+  {
+    let named = TestX4NamedWithGenerics{ x : 1, y : 2, z : 3, w : 4 };
+    assert!( implements!( named => X4NominalInterface ) );
+    assert!( implements!( named => X4Interface ) );
+    assert!( implements!( named => X4BasicInterface ) );
+    assert!( implements!( named => X4CanonicalInterface ) );
+  }
 }
 
 //
@@ -140,4 +188,8 @@ tests_index!
   impls_x3_for_tuple_with_generics,
   impls_x3_for_named,
   impls_x3_for_named_with_generics,
+  impls_x4_for_tuple,
+  impls_x4_for_tuple_with_generics,
+  impls_x4_for_named,
+  impls_x4_for_named_with_generics,
 }
