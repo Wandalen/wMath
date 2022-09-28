7e0742df849310a7fafe33f3e4adc68386caa413
Serhii Chushyn <chushin.sergey@gmail.com>
2022-09-28 14:37:15 +0300

    Add tests for macro with 4 dimension vectors

--- rust/test/math/adapter/impl_interfaces_macro_test.rs
+++ rust/test/math/adapter/impl_interfaces_macro_test.rs
@@ -3,6 +3,7 @@ use math_adapter::
 {
   X2,
   X3,
+  X4,
 };
 use super::*;
 
@@ -89,6 +90,48 @@ tests_impls!
     assert!( implements!( tuple => X3BasicInterface ) );
     assert!( implements!( tuple => X3CanonicalInterface ) );
   }
+
+  //
+
+    fn impls_x4_for_structure()
+  {
+    let x4 = X4( 1, 2, 3, 4 );
+    assert!( implements!( x4 => X4NominalInterface ) );
+    assert!( implements!( x4 => X4Interface ) );
+    assert!( implements!( x4 => X4BasicInterface ) );
+    assert!( implements!( x4 => X4CanonicalInterface ) );
+  }
+
+  //
+
+  fn impls_x4_for_array()
+  {
+    let array = [ 1, 2, 3, 4 ];
+    assert!( implements!( array => X4NominalInterface ) );
+    assert!( implements!( array => X4Interface ) );
+    assert!( implements!( array => X4BasicInterface ) );
+    assert!( implements!( array => X4CanonicalInterface ) );
+  }
+
+  //
+
+  fn impls_x4_for_slice()
+  {
+    let slice = &[ 1, 2, 3, 4 ][..];
+    assert!( implements!( slice => X4NominalInterface ) );
+    assert!( implements!( slice => X4Interface ) );
+  }
+
+  //
+
+  fn impls_x4_for_tuple()
+  {
+    let tuple = ( 1, 2, 3, 4 );
+    assert!( implements!( tuple => X4NominalInterface ) );
+    assert!( implements!( tuple => X4Interface ) );
+    assert!( implements!( tuple => X4BasicInterface ) );
+    assert!( implements!( tuple => X4CanonicalInterface ) );
+  }
 }
 
 //
@@ -103,4 +146,8 @@ tests_index!
   impls_x3_for_array,
   impls_x3_for_slice,
   impls_x3_for_tuple,
+  impls_x4_for_structure,
+  impls_x4_for_array,
+  impls_x4_for_slice,
+  impls_x4_for_tuple,
 }
